25e5f08284866d842abaf00b80e90eeaba7ef041
SaiTadikonda97 <saitadikonda19@gmail.com>
2016-06-21 03:11:22 -0400

    Adding App

--- srvup/settings.py
+++ srvup/settings.py
@@ -107,12 +107,12 @@ USE_L10N = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join('/Users/saitadikonda/Desktop/srvup/static/static_root'),
+STATICFILES_DIRS = os.path.join('/Users/saitadikonda/Desktop/srvup/static/static_root')
     # os.path.join(os.path.dirname(BASE_DIR), "srvup/src/static", "static_dirs"),
     #'/Users/jmitch/Desktop/srvup/static/static_dirs/', #on mac
     #'\Users\jmitch\Desktop\srvup\static\static_dirs\', somethingl ike this on windows
     #'/var/www/static/',
-)
+
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static_root")
 
