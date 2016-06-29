b7607b9163ff52bfa0f1306de5a907c510092138
SaiTadikonda97 <saitadikonda19@gmail.com>
2016-06-29 04:50:55 -0400

    Stuff

--- srvup/settings.py
+++ srvup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'lwi1a%uc6h1h0j@#3a1ys(&^m!*q!ulqtfbm3ts)_vsud^*=k%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # FULL_DOMAIN_NAME = "http://www.testpound.com"
